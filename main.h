46780f5cf9c69c405cfaccf8896e746f5dbfed43
amine <aminealouani2018@gmail.com>
2022-10-17 13:50:07 -0700

    HEADERS

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+void integer(int value);
+#endif
