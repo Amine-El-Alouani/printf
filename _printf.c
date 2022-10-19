355fb1a09dc9f3cb42951bc1f2298fee59a31b4a
amine <aminealouani2018@gmail.com>
2022-10-19 14:36:22 -0700

    Update

--- _printf.c
+++ _printf.c
@@ -8,59 +8,44 @@
 
 int _printf(const char *format, ...)
 {
-	int i;
-	int value;
-	char *val;
-	char vall;
-	int length;
-	int count = 0;
-	va_list ptr;
-	
-	length = strlen(format);
-	va_start(ptr, format);
+	unsigned int i = 0, len = 0, ibuf = 0;
+	va_list arguments;
+	int (*function)(va_list, char *, unsigned int);
+	char *buffer;
 
-	if (format == NULL)
-	{
+	va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
+	if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
 		return (-1);
-	}
-
-	for (i = 0; i < length; i++)
+	if (!format[i])
+		return (0);
+	for (i = 0; format && format[i]; i++)
 	{
 		if (format[i] == '%')
 		{
-		switch (format[i + 1])
-		{
-			case ('d'):
-				value = va_arg(ptr, int);
-				count += count_integer(value);
-				integer(value);
-				break;
-			case ('i'):
-				value = va_arg(ptr, int);
-				count += count_integer(value);
-				integer(value);
-				break;
-			case ('s'):
-				val = va_arg(ptr, char *);
-				count += strlen(val) - 1;
-				string(val);
-				break;
-			case ('%'):
-				_putchar('%');
-				break;
-			case('c'):
-				vall = (char) va_arg(ptr, int);
-				count++;
-				_putchar(vall);
-				break;
-			default:
-				i -= 1;
+			if (format[i + 1] == '\0')
+			{	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
+				return (-1);
+			}
+			else
+			{	function = get_print_func(format, i + 1);
+				if (function == NULL)
+				{
+					if (format[i + 1] == ' ' && !format[i + 2])
+						return (-1);
+					handl_buf(buffer, format[i], ibuf), len++, i--;
+				}
+				else
+				{
+					len += function(arguments, buffer, ibuf);
+					i += ev_print_func(format, i + 1);
+				}
+			} i++;
 		}
-		i  = i + 2;
+		else
+			handl_buf(buffer, format[i], ibuf), len++;
+		for (ibuf = len; ibuf > 1024; ibuf -= 1024)
+			;
 	}
-		if (i < length)
-			_putchar(format[i]);
-		count++;
-    }
-	return (count);
+	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
+	return (len);
 }
